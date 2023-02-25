8c845899e6a2836a14a3b0267a5c4a87154ba3f7
Yasotha Ramamoorthy <94780018+YasothaRamamoorthy@users.noreply.github.com>
2023-02-25 22:18:00 +0530

    Update bank.cpp

--- projectcpp/bank.cpp
+++ projectcpp/bank.cpp
@@ -1,25 +1,88 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class bank{
-int amount;
+class Account {
+private:
+    string name;
+    float balance;
+
 public:
-bank(int a){
-amount=a;
-}
-void withdrawing(int a)
-{
-    amount=amount-a;
-}
-void deposit(int a){
-    amount=amount+a;
-}
+    Account(string name, float balance = 0) {
+        this->name = name;
+        this->balance = balance;
+    }
+
+    void deposit(float amount) {
+        if (amount < 0) {
+            cout << "Invalid amount" << endl;
+        }
+        else {
+            balance += amount;
+            cout << "Deposit successful. Current balance is: " << balance << endl;
+        }
+    }
+
+    void withdraw(float amount) {
+        if (amount > balance) {
+            cout << "Insufficient balance" << endl;
+        }
+        else {
+            balance -= amount;
+            cout << "Withdrawal successful. Current balance is: " << balance << endl;
+        }
+    }
+
+    float getBalance() {
+        return balance;
+    }
 };
 
+class SavingsAccount : public Account {
+private:
+    float interest_rate;
 
-int main()
-{
-    bank customer1=new bank(500);
-    printf(customer1->amount);
+public:
+    SavingsAccount(string name, float balance = 0, float interest_rate = 0.01)
+        : Account(name, balance) {
+        this->interest_rate = interest_rate;
+    }
+
+    void add_interest() {
+        float interest = getBalance() * interest_rate;
+        deposit(interest);
+        cout << "Interest added. Current balance is: " << getBalance() << endl;
+    }
+};
+
+class CurrentAccount : public Account {
+private:
+    float overdraft_limit;
+
+public:
+    CurrentAccount(string name, float balance = 0, float overdraft_limit = 1000)
+        : Account(name, balance) {
+        this->overdraft_limit = overdraft_limit;
+    }
+
+    void withdraw(float amount) {
+        if (amount > getBalance() + overdraft_limit) {
+            cout << "Overdraft limit exceeded" << endl;
+        }
+        else {
+            balance -= amount;
+            cout << "Withdrawal successful. Current balance is: " << getBalance() << endl;
+        }
+    }
+};
+
+int main() {
+    SavingsAccount savings_acc("Alice", 1000);
+    savings_acc.deposit(500);
+    savings_acc.add_interest();
+
+    CurrentAccount current_acc("Bob", 500);
+    current_acc.withdraw(700);
+    current_acc.withdraw(800);
+
+    return 0;
 }
-
